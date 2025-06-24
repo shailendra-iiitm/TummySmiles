266806649597d50a388fcf981b58d43a94f6972c
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-25 01:47:13 +0530

    Added middleware to verifye roles

---
+++ backend/middlewares/verifyeRole.js
@@ -0,0 +1,9 @@
+// middleware/verifyRole.js
+module.exports = function(requiredRole) {
+  return (req, res, next) => {
+    if (req.user.role !== requiredRole) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+    next();
+  };
+};
